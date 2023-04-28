29eb5b05cb9b061535e6e133254622aa4e5d303e
JoCode <46027270+JoCode-dev@users.noreply.github.com>
2023-04-28 15:49:18 +0000

    Fix deploy error

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/Listings/ListingCard";
 
+export const dynamic = "force-dynamic";
+
 interface HomeProps {
   searchParams: IListingsParams;
 }
